739c7ef56eb2372fa00292dc906ca9be342d8d84
Joan Lledó <jlledo@redhat.com>
2023-10-04 13:55:23 +0200

    Remove fakeredis gem
    Not compatible with redis-client

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,9 @@ require "simplecov"
 SimpleCov.start
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'fakeredis/rspec'
 require 'sidekiq/batch'
 
-redis_opts = { url: "redis://127.0.0.1:6379/1" }
-redis_opts[:driver] = Redis::Connection::Memory if defined?(Redis::Connection::Memory)
+redis_opts = { url: "redis://127.0.0.1:6379/2" }
 
 Sidekiq.configure_client do |config|
   config.redis = redis_opts
@@ -19,6 +17,23 @@ end
 RSpec.configure do |config|
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
+
+  config.around(:each) do |example|
+    with_clean_redis do
+      example.run
+    end
+  end
+
+  def with_clean_redis
+    Sidekiq.redis do |r|
+      r.flushdb
+      begin
+        yield
+      ensure
+        r.flushdb
+      end
+    end
+  end
 end
 
 Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f }
